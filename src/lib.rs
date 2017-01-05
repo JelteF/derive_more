aea17abeb103e23c785840a01bbf030572942f95
Jelte Fennema <github-tech@jeltef.nl>
2017-01-05 20:14:39 +0100

    Fix build issues in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,13 @@
 
 #[macro_use]
 extern crate rustc;
-extern crate rustc_front;
 extern crate rustc_plugin;
 #[macro_use]
 extern crate syntax;
 extern crate syntax_ext;
 
 use rustc_plugin::Registry;
-use syntax::parse::token::intern;
+use syntax::symbol::Symbol;
 use syntax::ext::base::MultiDecorator;
 
 mod from;
@@ -21,18 +20,18 @@ const MULLIKE_OPS: &'static [&'static str] = &["Mul", "Div", "Rem", "Shr", "Shl"
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_syntax_extension(intern("derive_From"), MultiDecorator(box from::expand));
+    reg.register_syntax_extension(Symbol::intern("derive_From"), MultiDecorator(box from::expand));
     for op in ADDLIKE_OPS {
         let expand = move |cx: &mut ExtCtxt, span: Span, _: &MetaItem, item: &Annotatable, push: &mut FnMut(Annotatable)| {
             add_like::expand(cx, span, item, push, op)
         };
-        reg.register_syntax_extension(intern(&format!("derive_{}", op)), MultiDecorator(box expand));
+        reg.register_syntax_extension(Symbol::intern(&format!("derive_{}", op)), MultiDecorator(box expand));
     }
     for op in MULLIKE_OPS {
         let expand = move |cx: &mut ExtCtxt, span: Span, _: &MetaItem, item: &Annotatable, push: &mut FnMut(Annotatable)| {
             mul_like::expand(cx, span, item, push, op)
         };
-        reg.register_syntax_extension(intern(&format!("derive_{}", op)), MultiDecorator(box expand));
+        reg.register_syntax_extension(Symbol::intern(&format!("derive_{}", op)), MultiDecorator(box expand));
     }
 }
 
