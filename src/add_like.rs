549d45d5d7e3ab0786969efbc0c5bdc920c764dc
Jelte Fennema <github-tech@jeltef.nl>
2017-01-08 17:44:30 +0100

    Macros 1.1: Get struct add_like working (only for Add for now)

--- src/add_like.rs
+++ src/add_like.rs
@@ -3,6 +3,7 @@ use syn::{Body, Field, Ident, Variant, VariantData, MacroInput, Ty};
 
 pub fn expand(input: &MacroInput) -> Tokens {
     let trait_name = "Add";
+    let trait_ident = Ident::from(trait_name);
     let method_name = trait_name.to_lowercase();
     let method_ident = Ident::from(method_name.clone());
     let input_type = &input.ident;
@@ -11,11 +12,15 @@ pub fn expand(input: &MacroInput) -> Tokens {
         Body::Struct(VariantData::Tuple(ref fields)) => {
             (input_type, tuple_content(input_type, fields, &method_ident))
         },
+        Body::Struct(VariantData::Struct(ref fields)) => {
+            (input_type, struct_content(input_type, fields, &method_ident))
+        },
+
         _ => panic!(format!("Only structs and enums can use dervie({})", trait_name))
     };
 
     quote!(
-        impl std::ops::#trait_name for #input_type {
+        impl std::ops::#trait_ident for #input_type {
             type Output = #output_type;
             fn #method_ident(self, rhs: #input_type) -> #output_type {
                 #block
@@ -30,13 +35,23 @@ fn tuple_content(input_type: &Ident, fields: &Vec<Field>, method_ident: &Ident) 
     for i in 0..fields.len() {
         let i = Ident::from(i.to_string());
         let expr = quote!(self.#i.#method_ident(rhs.#i));
-        println!("{:?}", expr);
         exprs.push(expr);
-        return quote!(#input_type(#expr));
     }
 
-    let call = quote!(#input_type(#exprs));
-    println!("{:?}", call);
-    call
+    quote!(#input_type(#(#exprs),*))
+}
+
+
+fn struct_content(input_type: &Ident, fields: &Vec<Field>, method_ident: &Ident) -> Tokens  {
+    let mut exprs = vec![];
+
+    for field in fields {
+        // It's safe to unwrap because struct fields always have an identifier
+        let field_id = field.ident.clone().unwrap();
+        let expr = quote!(#field_id: self.#field_id.#method_ident(rhs.#field_id));
+        exprs.push(expr)
+    }
+
+    quote!(#input_type{#(#exprs),*})
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,17 +2,18 @@
 extern crate derive_more;
 
 #[derive(From)]
-//#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug)]
 #[derive(Add)]
 struct MyInt(i32);
 
-// #[derive(Add)]
-// #[derive(Eq, PartialEq, Debug)]
-// struct MyUInt(u64, u64);
+#[derive(Add)]
+#[derive(Eq, PartialEq, Debug)]
+struct MyUInt(u64, u64);
 // 
 // #[derive(Add, Sub, Mul, Div, Rem, BitAnd, BitOr, BitXor, Shr, Shl)]
-// #[derive(Eq, PartialEq, Debug, Clone, Copy)]
-// struct NormalStruct{int1: u64, int2: u64}
+#[derive(Eq, PartialEq, Debug, Clone, Copy)]
+#[derive(Add)]
+struct NormalStruct{int1: u64, int2: u64}
 // 
 // #[derive(From)]
 // //#[derive(Eq, PartialEq, Debug)]
@@ -75,12 +76,12 @@ fn main() {
     assert_eq!(MyInt(5), 5.into());
     // assert_eq!(MyIntEnum::SmallInt(5), 5.into());
 
-    // assert_eq!(MyInt(4) + MyInt(1), 5.into());
-    // assert_eq!(MyUInt(4, 5) + MyUInt(1, 2), MyUInt(5, 7));
-    // let s1 = NormalStruct{int1: 1, int2: 2};
-    // let s2 = NormalStruct{int1: 2, int2: 3};
-    // let s3 = NormalStruct{int1: 3, int2: 5};
-    // assert_eq!(s1 + s2, s3);
+    assert_eq!(MyInt(4) + MyInt(1), 5.into());
+    assert_eq!(MyUInt(4, 5) + MyUInt(1, 2), MyUInt(5, 7));
+    let s1 = NormalStruct{int1: 1, int2: 2};
+    let s2 = NormalStruct{int1: 2, int2: 3};
+    let s3 = NormalStruct{int1: 3, int2: 5};
+    assert_eq!(s1 + s2, s3);
     // assert_eq!(s3 - s2, s1);
     // assert_eq!((SimpleMyIntEnum::Int(6) + 5.into()).unwrap(), 11.into());
     // assert_eq!((SimpleMyIntEnum::Int(6) - 5.into()).unwrap(), 1.into());
