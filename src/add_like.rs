3a7a8331663e1a3b005e9fd06a1fe990a8bbcae9
Jelte Fennema <github-tech@jeltef.nl>
2017-01-08 11:05:45 +0100

    Macros 1.1: Continue with add_like

--- src/add_like.rs
+++ src/add_like.rs
@@ -5,22 +5,22 @@ pub fn expand(input: &MacroInput) -> Tokens {
     let trait_name = "Add";
     let method_name = trait_name.to_lowercase();
     let method_ident = Ident::from(method_name);
-    let result = match input.body {
+    let input_type = input.ident;
+    let (output_type, block) = match input.body {
         Body::Struct(VariantData::Tuple(ref fields)) => {
-            Some((input.ident, tuple_content(cx, span, x, fields, method_name)))
+            (input_type, tuple_content(input_type, fields, method_name))
         },
-        _ => None,
+        _ => panic!(format!("Only structs and enums can use dervie({})", trait_name))
     };
     quote!()
 }
 
-fn tuple_content(item: Item, fields: &Vec<StructField>, method_name: String) -> Tokens {
-    let type_name = item.ident;
-    let mut exprs: = vec![];
+fn tuple_content(input_type: Ident, fields: &Vec<Field>, method_name: String) -> Tokens  {
+    let mut exprs = vec![];
 
     for i in 0..fields.len() {
         let i = &i.to_string();
-        exprs.push(cx.parse_expr(format!("self.{}.{}(rhs.{})", i, method_name, i)));
+        exprs.push(quote!((format!("self.{}.{}(rhs.{})", i, method_name, i)));
     }
 
     cx.expr_call_ident(span, type_name, exprs)
