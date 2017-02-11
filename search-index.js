3d33b35d1e61df5fad60766171613577348bac43
Jelte Fennema <github-tech@jeltef.nl>
2017-02-11 14:01:03 +0100

    Add generated docs

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["derive_more"] = {"doc":"# derive_more\nRust has lots of builtin traits that are implemented for its basic types, such as [`Add`],\n[`Not`] or [`From`].\nHowever, when wrapping these types inside your own structs or enums you lose the\nimplementations of these traits and are required to recreate them.\nThis is especially annoying when your own structures are very simple, such as when using the\ncommonly advised newtype pattern (e.g. `MyInt(i32)`).","items":[],"paths":[]};
+initSearch(searchIndex);
