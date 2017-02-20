071fef85362c0487daf1ae1916b71e0e22a386ab
Jelte Fennema <github-tech@jeltef.nl>
2017-02-20 22:53:40 +0100

    Update for 0.6.0 release

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors["quote"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='core::convert::AsRef'>AsRef</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='struct' href='quote/struct.Ident.html' title='quote::Ident'>Ident</a>",];
-implementors["syn"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='core::convert::AsRef'>AsRef</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='struct' href='syn/struct.Ident.html' title='syn::Ident'>Ident</a>",];
+implementors["quote"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='trait core::convert::AsRef'>AsRef</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='struct' href='quote/struct.Ident.html' title='struct quote::Ident'>Ident</a>",];
+implementors["syn"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='trait core::convert::AsRef'>AsRef</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='struct' href='syn/struct.Ident.html' title='struct syn::Ident'>Ident</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
