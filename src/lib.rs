7a4c559c89c1e1bc81a181b9fe7a6fdf19f0ca03
Jelte Fennema <github-tech@jeltef.nl>
2018-03-14 10:06:46 +0100

    Docs: Correct link

--- src/lib.rs
+++ src/lib.rs
@@ -73,7 +73,7 @@
 //! 1. [`#[derive(From)]`](from.html)
 //! 2. [`#[derive(Into)]`](into.html)
 //! 3. [`#[derive(Constructor)]`](constructor.html)
-//! 4. [`#[derive(FromStr)]`](fromstr.html)
+//! 4. [`#[derive(FromStr)]`](from_str.html)
 //! 5. [`#[derive(Not)]`](not.html)
 //! 6. [`#[derive(Add)]`](add.html)
 //! 7. [`#[derive(AddAssign)]`](add_assign.html)
