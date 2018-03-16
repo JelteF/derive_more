e4a3ae4b4b6bc9bb8e5b3fac79fbbdee22ffa100
Jelte Fennema <github-tech@jeltef.nl>
2018-03-16 09:11:00 +0100

    Tests: Add generics test for Display

--- tests/generics.rs
+++ tests/generics.rs
@@ -2,13 +2,13 @@
 #[macro_use]
 extern crate derive_more;
 
-#[derive(From, Not, Add, AddAssign, Mul, FromStr)]
+#[derive(From, Not, Add, AddAssign, Mul, FromStr, Display)]
 struct Wrapped<T: Clone>(T);
 
 #[derive(From, Not, Add, AddAssign, Mul)]
 struct WrappedDouble<T: Clone, U: Clone>(T, U);
 
-#[derive(From, Not, Add, AddAssign, Mul, FromStr)]
+#[derive(From, Not, Add, AddAssign, Mul, FromStr, Display)]
 struct Struct<T: Clone> {
     t: T,
 }
