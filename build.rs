576850fd464ecf7854f314d0824c4dd77679d8a4
Jelte Fennema <github-tech@jeltef.nl>
2020-03-28 13:07:52 +0100

    Automatically detect nightly feature in testing

--- build.rs
+++ build.rs
@@ -1,23 +1,14 @@
+#[cfg(feature = "testing-helpers")]
 extern crate rustc_version;
 
 #[cfg(feature = "generate-parsing-rs")]
 extern crate peg;
 
-use rustc_version::{version_meta, Channel};
 
 #[cfg(not(feature = "generate-parsing-rs"))]
-fn main() {
-    if version_meta().unwrap().channel == Channel::Nightly {
-        println!("cargo:rustc-cfg=feature=\"nightly\"");
-    }
-}
-
+fn generate_peg() {}
 #[cfg(feature = "generate-parsing-rs")]
-fn main() {
-    if version_meta().unwrap().channel == Channel::Nightly {
-        println!("cargo:rustc-cfg=feature=\"nightly\"");
-    }
-
+fn generate_peg() {
     let contents = match ::std::fs::read_to_string("src/parsing.rustpeg") {
         Ok(contents) => contents,
         Err(e) => panic!("{}", e),
@@ -31,4 +22,20 @@ fn main() {
     if let Err(e) = ::std::fs::write("src/parsing.rs", compiled) {
         panic!("{}", e);
     }
+}
+
+#[cfg(not(feature = "testing-helpers"))]
+fn detect_nightly() {}
+
+#[cfg(feature = "testing-helpers")]
+fn detect_nightly() {
+    use rustc_version::{version_meta, Channel};
+    if version_meta().unwrap().channel == Channel::Nightly {
+        println!("cargo:rustc-cfg=feature=\"nightly\"");
+    }
+}
+
+fn main() {
+    detect_nightly();
+    generate_peg();
 }
--- ci/test_all_features.sh
+++ ci/test_all_features.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|nightly\|generate-parsing-rs'); do
-    cargo test --tests --no-default-features --features  "$feature";
+    cargo test --tests --no-default-features --features  "$feature,testing-helpers";
 done
 
