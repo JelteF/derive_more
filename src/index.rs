fc802d3e96b95957a77ca17dac4f0c04a707b893
Jelte Fennema <github-tech@jeltef.nl>
2020-06-29 23:41:18 +0200

    Refactor a bit for more code sharing

--- src/index.rs
+++ src/index.rs
@@ -1,10 +1,10 @@
 use crate::utils::{
-    add_extra_generic_param, add_extra_where_clauses, add_where_clauses_for_new_ident,
-    DeriveType, MultiFieldData, MultiVariantData, SingleFieldData, State,
+    add_extra_generic_param, add_extra_where_clauses,
+    DeriveType, MultiVariantData, SingleFieldData, State,
 };
 use proc_macro2::{Span, TokenStream};
-use quote::{quote, ToTokens};
-use syn::{parse::Result, DeriveInput, Ident};
+use quote::{quote};
+use syn::{parse::Result, DeriveInput, Generics, Ident};
 
 /// Provides the hook to expand `#[derive(Index)]` into an implementation of `Index`
 pub fn expand(input: &DeriveInput, trait_name: &'static str) -> Result<TokenStream> {
@@ -14,37 +14,60 @@ pub fn expand(input: &DeriveInput, trait_name: &'static str) -> Result<TokenStre
         quote!(::core::ops),
         trait_name.to_lowercase(),
     )?;
+    let index_type = &Ident::new("__IdxT", Span::call_site());
+    state.add_trait_path_type_param(quote!(#index_type));
+    let output_type = &Ident::new("__IdxOutputT", Span::call_site());
+    let mut new_generics =
+        add_extra_generic_param(&input.generics, quote!(#index_type));
+    new_generics = add_extra_generic_param(&new_generics, quote!(#output_type));
+
     if state.derive_type == DeriveType::Enum {
-        Ok(enum_index(input, state))
+        Ok(enum_index(input, state, new_generics, index_type, output_type))
     } else {
-        Ok(struct_index(input, &mut state))
+        Ok(struct_index(input, &mut state, new_generics, index_type, output_type))
     }
 }
 
-fn struct_index(input: &DeriveInput, state: &mut State) -> TokenStream {
-    let index_type = &Ident::new("__IdxT", Span::call_site());
-    state.add_trait_path_type_param(quote!(#index_type));
+fn index_body(
+    single_field_data: &SingleFieldData,
+    mut new_generics: Generics,
+    index_type: &Ident,
+    output_type: &Ident,
+) -> (TokenStream, Generics) {
     let SingleFieldData {
-        field,
+        casted_trait,
         field_type,
+        trait_path,
+        ..
+    } = single_field_data;
+
+    let type_where_clauses = quote! {
+        where #field_type: #trait_path<#index_type, Output=#output_type>
+    };
+
+    let body = quote!(#casted_trait::index(indexable, idx));
+    new_generics = add_extra_where_clauses(&new_generics, type_where_clauses);
+    (body, new_generics)
+}
+
+fn struct_index(
+    input: &DeriveInput,
+    state: &State,
+    new_generics: Generics,
+    index_type: &Ident,
+    output_type: &Ident,
+) -> TokenStream {
+    let single_field_data = state.assert_single_enabled_field();
+
+    let (body, new_generics) =
+        index_body(&single_field_data, new_generics, index_type, output_type);
+    let SingleFieldData {
         input_type,
         trait_path_with_params,
         casted_trait,
         member,
         ..
-    } = state.assert_single_enabled_field();
-
-    let type_where_clauses = quote! {
-        where #field_type: #trait_path_with_params
-    };
-
-    let new_generics = add_where_clauses_for_new_ident(
-        &input.generics,
-        &[field],
-        index_type,
-        type_where_clauses,
-        true,
-    );
+    } = single_field_data;
 
     let (impl_generics, _, where_clause) = new_generics.split_for_impl();
     let (_, ty_generics, _) = input.generics.split_for_impl();
@@ -54,19 +77,20 @@ fn struct_index(input: &DeriveInput, state: &mut State) -> TokenStream {
             type Output = #casted_trait::Output;
             #[inline]
             fn index(&self, idx: #index_type) -> &Self::Output {
-                #casted_trait::index(&#member, idx)
+                let indexable = &#member;
+                #body
             }
         }
     }
 }
 
-fn enum_index(input: &DeriveInput, mut state: State) -> TokenStream {
-    let index_type = &Ident::new("__IdxT", Span::call_site());
-    let output_type = &Ident::new("__IdxOutputT", Span::call_site());
-    let mut new_generics =
-        add_extra_generic_param(&input.generics, quote!(#index_type));
-    new_generics = add_extra_generic_param(&new_generics, quote!(#output_type));
-    state.add_trait_path_type_param(quote!(#index_type));
+fn enum_index(
+    input: &DeriveInput,
+    state: State,
+    mut new_generics: Generics,
+    index_type: &Ident,
+    output_type: &Ident,
+) -> TokenStream {
     let MultiVariantData {
         input_type,
         variant_states,
@@ -82,22 +106,12 @@ fn enum_index(input: &DeriveInput, mut state: State) -> TokenStream {
         if first_single_field_data.is_none() {
             first_single_field_data = Some(single_field_data.clone());
         }
-        let matched = &quote!(matched);
-        patterns.push(single_field_data.matcher(&[0], &[matched]));
-
-        let SingleFieldData {
-            casted_trait,
-            field_type,
-            trait_path,
-            ..
-        } = single_field_data;
-
-        let type_where_clauses = quote! {
-            where #field_type: #trait_path<#index_type, Output=#output_type>
-        };
-
-        bodies.push(quote!(#casted_trait::index(#matched, idx)));
-        new_generics = add_extra_where_clauses(&new_generics, type_where_clauses);
+        let indexable = &quote!(indexable);
+        patterns.push(single_field_data.matcher(&[0], &[indexable]));
+        let (body, temp_new_generics) =
+            index_body(&single_field_data, new_generics, index_type, output_type);
+        new_generics = temp_new_generics;
+        bodies.push(body);
     }
 
     let (impl_generics, _, where_clause) = new_generics.split_for_impl();
