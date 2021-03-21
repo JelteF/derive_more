e7071139891886b15784125b28dfc57147abdc4d
Jelte Fennema <github-tech@jeltef.nl>
2021-03-21 17:41:34 +0100

    Update doc build script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Go to latest release
+latest_tag_hash=$(git rev-list --tags --max-count=1)
+latest_tag=$(git describe "$latest_tag_hash")
+git checkout "$latest_tag"
+
+# build docs
 rm -rf target/debug
 cargo +nightly build
 cargo +nightly external-doc
+# go back to old branch
+git checkout -
+
+# Push doc changes
 cd gh-pages
 git add .
-git commit -m 'Updated docs to latest version'
+git commit -m "Update docs for $latest_tag release"
 git push
