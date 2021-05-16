11c592400cbbe1fad1c2a980bc691c971928bde1
Jelte Fennema <github-tech@jeltef.nl>
2021-05-16 15:25:35 +0200

    Update doc build script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -6,6 +6,21 @@ latest_tag_hash=$(git rev-list --tags --max-count=1)
 latest_tag=$(git describe "$latest_tag_hash")
 git checkout "$latest_tag"
 
+# Make sure we have gh-pages directory
+if [ ! -d gh-pages ]; then
+    git clone git@github.com:JelteF/derive_more --branch gh-pages gh-pages
+fi
+
+# Make sure the gh-pages directory has the latest commits
+(
+    cd gh-pages
+    git fetch
+    git checkout gh-pages
+    git reset --hard origin/gh-pages
+)
+
+# Remove old docs
+rm -rf gh-pages/*
 # build docs
 rm -rf target/debug
 cargo +nightly build
@@ -14,7 +29,12 @@ cargo +nightly external-doc
 git checkout -
 
 # Push doc changes
-cd gh-pages
-git add .
-git commit -m "Update docs for $latest_tag release"
-git push
+(
+    cd gh-pages
+    git add .
+    git commit -m "Update docs for $latest_tag release"
+    git push
+)
+
+# Go back to previous commit instead of latest tag
+git checkout -
