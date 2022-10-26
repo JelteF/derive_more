9eacb0a4d420dc667d61835d8d0089e1dedf25fd
tyranron <tyranron@gmail.com>
2022-10-26 16:31:56 +0200

    Explicitly test either whole workspace or a single crate on CI

--- ci/test_all_features.sh
+++ ci/test_all_features.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|testing-helpers'); do
-    cargo test --tests --no-default-features --features "$feature,testing-helpers";
+    cargo test -p derive_more --tests --no-default-features --features "$feature,testing-helpers";
 done
