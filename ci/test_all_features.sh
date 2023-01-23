8d67ddf07df4761e4fbab02e0c2e3f86a1c263c6
tyranron <tyranron@gmail.com>
2023-01-23 16:27:32 +0200

    Investigate, vol.5

--- ci/test_all_features.sh
+++ ci/test_all_features.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -eux
 
 for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|testing-helpers'); do
     cargo test -p derive_more --tests --no-default-features --features "$feature,testing-helpers";
