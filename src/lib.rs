JelteF/derive_more

commit aa4d5eb0e72b69258860e605d39b9202ff820bcc

Disable all features by default (#234, #210)
As discussed in #210 we want to disable all features by default in 1.0. With the amount of derives that are part of this library, the compile time of the library itself becomes significant. This way, we don't have to worry about increasing people their compile time when adding more derives.

On my machine, compiling the `derive_more-impl` crate took ~4.5 seconds when enabling all features. But when only enabling a single derive, it took 1.5 seconds.

Enabling all derives is still easy to do, by using the `full` feature.

.github/workflows/ci.yml
@@ -73,7 +73,7 @@ jobs:
       - name: Install minimal dependencies versions
         run: cargo +nightly update -Z minimal-versions
 
-      - run: cargo test --workspace --features testing-helpers
+      - run: cargo test --workspace --features full,testing-helpers
 
   no_std:
     runs-on: ubuntu-latest
@@ -92,7 +92,7 @@ jobs:
              #       https://github.com/japaric/trust/pull/137
 
       - run: cargo nono check --package derive_more
-                   --no-default-features --features all_no_std
+                   --no-default-features --features full
 
   test:
     strategy:

src/lib.rs
@@ -26,7 +26,18 @@
 //! [`Constructor`]: crate::Constructor
 //! [`IsVariant`]: crate::IsVariant
 //! [`Unwrap`]: crate::Unwrap
-#![doc = include_str!("../README.md")]
+// The README includes doctests requiring these features. To make sure that
+// tests pass when not all features are provided we exclude it when the
+// required features are not available.
+#![cfg_attr(
+    all(
+        feature = "add",
+        feature = "display",
+        feature = "from",
+        feature = "into"
+    ),
+    doc = include_str!("../README.md")
+)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![deny(rustdoc::broken_intra_doc_links, rustdoc::private_intra_doc_links)]
 #![forbid(non_ascii_idents, unsafe_code)]

Cargo.toml
@@ -39,6 +39,8 @@ github = { repository = "JelteF/derive_more", workflow = "CI" }
 rustdoc-args = ["--cfg", "docsrs"]
 
 [features]
+default = ["std"]
+
 add_assign = ["derive_more-impl/add_assign"]
 add = ["derive_more-impl/add"]
 as_mut = ["derive_more-impl/as_mut"]
@@ -61,15 +63,10 @@ not = ["derive_more-impl/not"]
 sum = ["derive_more-impl/sum"]
 try_into = ["derive_more-impl/try_into"]
 is_variant = ["derive_more-impl/is_variant"]
-std = []
 unwrap = ["derive_more-impl/unwrap"]
 
-default = [
-    "all_no_std",
-    "std",
-]
-
-all_no_std = [
+std = []
+full = [
     "add_assign",
     "add",
     "as_mut",
@@ -145,7 +142,7 @@ required-features = ["display"]
 [[test]]
 name = "error"
 path = "tests/error_tests.rs"
-required-features = ["error"]
+required-features = ["error", "std"]
 
 [[test]]
 name = "from"
@@ -220,97 +217,19 @@ required-features = ["display"]
 [[test]]
 name = "no_std"
 path = "tests/no_std.rs"
-required-features = [
-    "add_assign",
-    "add",
-    "as_mut",
-    "as_ref",
-    "constructor",
-    "deref",
-    "deref_mut",
-    "display",
-    "from",
-    "from_str",
-    "index",
-    "index_mut",
-    "into",
-    "into_iterator",
-    "mul_assign",
-    "mul",
-    "not",
-    "sum",
-    "try_into",
-    "is_variant",
-]
+required-features = ["full"]
 
 [[test]]
 name = "generics"
 path = "tests/generics.rs"
-required-features = [
-    "add_assign",
-    "add",
-    "as_mut",
-    "as_ref",
-    "constructor",
-    "deref",
-    "deref_mut",
-    "display",
-    "from",
-    "from_str",
-    "index",
-    "index_mut",
-    "into",
-    "mul_assign",
-    "mul",
-    "not",
-    "try_into",
-    "is_variant",
-]
+required-features = ["full"]
 
 [[test]]
 name = "lib"
 path = "tests/lib.rs"
-required-features = [
-    "add_assign",
-    "add",
-    "as_mut",
-    "as_ref",
-    "constructor",
-    "deref",
-    "deref_mut",
-    "display",
-    "from",
-    "from_str",
-    "index",
-    "index_mut",
-    "into",
-    "mul_assign",
-    "mul",
-    "not",
-    "try_into",
-    "is_variant",
-]
+required-features = ["full"]
 
 [[example]]
 name = "deny_missing_docs"
 path = "examples/deny_missing_docs.rs"
-required-features = [
-    "add_assign",
-    "add",
-    "as_mut",
-    "as_ref",
-    "constructor",
-    "deref",
-    "deref_mut",
-    "display",
-    "from",
-    "from_str",
-    "index",
-    "index_mut",
-    "into",
-    "mul_assign",
-    "mul",
-    "not",
-    "try_into",
-    "is_variant",
-]
+required-features = ["full"]

impl/Cargo.toml
@@ -32,7 +32,7 @@ unicode-xid = { version = "0.2.2", optional = true }
 rustc_version = { version = "0.4", optional = true }
 
 [dev-dependencies]
-derive_more = { path = ".." }
+derive_more = { path = "..", features = ["add", "not", "try_into"] }
 itertools = "0.10.5"
 
 [badges]
@@ -42,6 +42,8 @@ github = { repository = "JelteF/derive_more", workflow = "CI" }
 rustdoc-args = ["--cfg", "docsrs"]
 
 [features]
+default = []
+
 add_assign = []
 add = []
 as_mut = []
@@ -49,10 +51,10 @@ as_ref = []
 constructor = []
 deref = []
 deref_mut = []
-display = ["syn/extra-traits", "unicode-xid"]
+display = ["syn/extra-traits", "dep:unicode-xid"]
 error = ["syn/extra-traits"]
 from = ["syn/extra-traits"]
-from_str = ["convert_case"]
+from_str = ["dep:convert_case"]
 index = []
 index_mut = []
 into = ["syn/extra-traits"]
@@ -63,9 +65,7 @@ mul = ["syn/extra-traits"]
 not = ["syn/extra-traits"]
 sum = []
 try_into = ["syn/extra-traits"]
-is_variant = ["convert_case"]
-unwrap = ["convert_case"]
+is_variant = ["dep:convert_case"]
+unwrap = ["dep:convert_case"]
 
-default = []
-
-testing-helpers = ["rustc_version"]
+testing-helpers = ["dep:rustc_version"]

commit 69fd1a6838499cf02bc481c90848f93f644eb7b9

Add `compile_error!` if no derive features enabled (#245) (#246)
Resolves #245



## Synopsis

From https://github.com/JelteF/derive_more/issues/245#issue-1613696886:
> In [#244
(comment)](https://github.com/JelteF/derive_more/issues/244#issuecomment-1458316209)
a user had an issue with the new approach of no default features
introduced by #234. To guide users we should probably add a clear error
if no features (other than `std`) are selected. Since that would result
in a useless dependency. In that error we could hint towards specifying
features or going for the `"full"` feature.




## Solution

Add `compile_error!()` to `derive_more` crate if no derive features were
specified.




## Checklist

- [x] ~~Documentation is updated~~ (not required)
- [x] ~~Tests are added/updated~~ (seems no easy way to test that)
- [x] ~~[CHANGELOG entry][l:1] is added~~ (not required)




[l:1]: /CHANGELOG.md

.github/workflows/ci.yml
@@ -112,7 +112,7 @@ jobs:
         with:
           toolchain: ${{ matrix.toolchain }}
 
-      - run: cargo test --workspace --features testing-helpers
+      - run: cargo test --workspace --features full,testing-helpers
 
   test-features:
     name: test features
@@ -145,7 +145,7 @@ jobs:
         with:
           toolchain: nightly
 
-      - run: cargo +nightly doc --workspace
+      - run: cargo +nightly doc --workspace --all-features
         env:
           RUSTDOCFLAGS: --cfg docsrs
 

src/lib.rs
@@ -62,3 +62,34 @@ pub mod ops;
 mod r#str;
 #[cfg(feature = "from_str")]
 pub use self::r#str::FromStrError;
+
+#[cfg(not(any(
+    feature = "full",
+    feature = "add_assign",
+    feature = "add",
+    feature = "as_mut",
+    feature = "as_ref",
+    feature = "constructor",
+    feature = "debug",
+    feature = "deref",
+    feature = "deref_mut",
+    feature = "display",
+    feature = "error",
+    feature = "from",
+    feature = "from_str",
+    feature = "index",
+    feature = "index_mut",
+    feature = "into",
+    feature = "into_iterator",
+    feature = "is_variant",
+    feature = "iterator",
+    feature = "mul_assign",
+    feature = "mul",
+    feature = "not",
+    feature = "sum",
+    feature = "try_into",
+    feature = "unwrap"
+)))]
+compile_error!(
+    "at least one derive feature must be enabled (or the \"full\" one enabling all the derives)"
+);

ci/test_all_features.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|testing-helpers'); do
+for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|std\|testing-helpers'); do
     cargo test -p derive_more --tests --no-default-features --features "$feature,testing-helpers";
 done
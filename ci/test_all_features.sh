69fd1a6838499cf02bc481c90848f93f644eb7b9
Kai Ren <tyranron@gmail.com>
2023-03-15 22:10:14 +0200

    Add `compile_error!` if no derive features enabled (#245) (#246)
    Resolves #245
    
    
    
    ## Synopsis
    
    From https://github.com/JelteF/derive_more/issues/245#issue-1613696886:
    > In [#244
    (comment)](https://github.com/JelteF/derive_more/issues/244#issuecomment-1458316209)
    a user had an issue with the new approach of no default features
    introduced by #234. To guide users we should probably add a clear error
    if no features (other than `std`) are selected. Since that would result
    in a useless dependency. In that error we could hint towards specifying
    features or going for the `"full"` feature.
    
    
    
    
    ## Solution
    
    Add `compile_error!()` to `derive_more` crate if no derive features were
    specified.
    
    
    
    
    ## Checklist
    
    - [x] ~~Documentation is updated~~ (not required)
    - [x] ~~Tests are added/updated~~ (seems no easy way to test that)
    - [x] ~~[CHANGELOG entry][l:1] is added~~ (not required)
    
    
    
    
    [l:1]: /CHANGELOG.md

--- ci/test_all_features.sh
+++ ci/test_all_features.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|testing-helpers'); do
+for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|std\|testing-helpers'); do
     cargo test -p derive_more --tests --no-default-features --features "$feature,testing-helpers";
 done
