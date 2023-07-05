cc40c3b266b8076e3f0cfbb1cc3ba6e597d786fb
tyranron <tyranron@gmail.com>
2023-07-05 14:28:55 +0300

    Minor corrections

--- ci/test_all_features.sh
+++ ci/test_all_features.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
+std=''
+if [ "${1:-}" = 'std' ]; then
+    std=',std'
+fi
+
 set -euxo pipefail
 
 for feature in $(tomljson Cargo.toml | jq --raw-output '.features | keys[]' | grep -v 'default\|std\|full\|testing-helpers'); do
-    if [ "${1:-}" = 'std' ]; then
-        cargo test -p derive_more --tests --no-default-features --features "$feature,std,testing-helpers";
-    else
-        cargo test -p derive_more --tests --no-default-features --features "$feature,testing-helpers";
-    fi
+    cargo +nightly test -p derive_more --tests --no-default-features --features "$feature$std,testing-helpers"
 done
