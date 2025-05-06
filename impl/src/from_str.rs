c8f70ce8df6aa7751cddc8e527b6358d9ee7ce9f
tyranron <tyranron@gmail.com>
2025-05-06 19:55:52 +0200

    New flat enum impl

--- impl/src/from_str.rs
+++ impl/src/from_str.rs
@@ -1,5 +1,6 @@
 //! Implementation of a [`FromStr`] derive macro.
 
+use std::collections::HashMap;
 #[cfg(doc)]
 use std::str::FromStr;
 
@@ -13,7 +14,9 @@ pub fn expand(input: &syn::DeriveInput, _: &'static str) -> syn::Result<TokenStr
         syn::Data::Struct(_) => {
             Ok(ForwardExpansion::try_from(input)?.into_token_stream())
         }
-        syn::Data::Enum(_data) => todo!(),
+        syn::Data::Enum(_) => {
+            Ok(EnumFlatExpansion::try_from(input)?.into_token_stream())
+        }
         syn::Data::Union(data) => Err(syn::Error::new(
             data.union_token.span(),
             "`FromStr` cannot be derived for unions",
@@ -61,19 +64,19 @@ impl<'i> TryFrom<&'i syn::DeriveInput> for ForwardExpansion<'i> {
         };
 
         Ok(Self {
-            inner,
             ident: &input.ident,
             generics: &input.generics,
+            inner,
         })
     }
 }
 
 impl ToTokens for ForwardExpansion<'_> {
-    /// Expands [`TryFrom`] implementations for a struct.
+    /// Expands a forwarding [`FromStr`] implementations for a struct.
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let ty = self.ident;
         let (impl_generics, ty_generics, where_clause) = self.generics.split_for_impl();
-        
+
         let inner_ty = &self.inner.ty;
         let constructor = if let Some(name) = &self.inner.ident {
             quote! { Self { #name: v } }
@@ -95,3 +98,94 @@ impl ToTokens for ForwardExpansion<'_> {
     }
 }
 
+/// Expansion of a macro for generating a flats [`FromStr`] implementation of an enum.
+struct EnumFlatExpansion<'i> {
+    /// [`syn::Ident`] of the enum.
+    ///
+    /// [`syn::Ident`]: struct@syn::Ident
+    ident: &'i syn::Ident,
+
+    /// [`syn::Generics`] of the enum.
+    generics: &'i syn::Generics,
+
+    /// [`syn::Ident`]s of the enum variants.
+    variants: Vec<&'i syn::Ident>,
+}
+
+impl<'i> TryFrom<&'i syn::DeriveInput> for EnumFlatExpansion<'i> {
+    type Error = syn::Error;
+
+    fn try_from(input: &'i syn::DeriveInput) -> syn::Result<Self> {
+        let syn::Data::Enum(data) = &input.data else {
+            return Err(syn::Error::new(
+                input.span(),
+                "expected an enum for flat `FromStr` derive",
+            ));
+        };
+
+        let variants = data
+            .variants
+            .iter()
+            .map(|variant| {
+                if !variant.fields.is_empty() {
+                    return Err(syn::Error::new(
+                        variant.fields.span(),
+                        "only enums with no fields can derive `FromStr`",
+                    ));
+                }
+                Ok(&variant.ident)
+            })
+            .collect::<syn::Result<_>>()?;
+
+        Ok(Self {
+            ident: &input.ident,
+            generics: &input.generics,
+            variants,
+        })
+    }
+}
+
+impl ToTokens for EnumFlatExpansion<'_> {
+    /// Expands a flat [`FromStr`] implementations for an enum.
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let ty = self.ident;
+        let (impl_generics, ty_generics, where_clause) = self.generics.split_for_impl();
+        let ty_name = ty.to_string();
+
+        let similar_lowercased = self
+            .variants
+            .iter()
+            .map(|v| v.to_string().to_lowercase())
+            .fold(<HashMap<_, u8>>::new(), |mut counts, v| {
+                *counts.entry(v).or_default() += 1;
+                counts
+            });
+
+        let match_arms = self.variants.iter().map(|variant| {
+            let name = variant.to_string();
+            let lowercased = name.to_lowercase();
+
+            let guard =
+                (similar_lowercased[&lowercased] > 1).then(|| quote! { if s == #name });
+            
+            quote! { #lowercased #guard=> Self::#variant, }
+        });
+
+        quote! {
+            #[automatically_derived]
+            impl #impl_generics derive_more::core::str::FromStr for #ty #ty_generics #where_clause {
+                type Err = derive_more::FromStrError;
+
+                #[inline]
+                fn from_str(s: &str) -> derive_more::core::result::Result<Self, Self::Err> {
+                    derive_more::core::result::Result::Ok(match s.to_lowercase().as_str() {
+                        #( #match_arms )*
+                        _ => return derive_more::core::result::Result::Err(
+                            derive_more::FromStrError::new(#ty_name),
+                        ),
+                    })
+                }
+            }
+        }.to_tokens(tokens);
+    }
+}
