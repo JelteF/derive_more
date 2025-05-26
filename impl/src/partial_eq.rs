0d998f8db1b84ac88b51df84f74c59763c837557
tyranron <tyranron@gmail.com>
2025-05-26 20:12:56 +0200

    Support empty fields

--- impl/src/partial_eq.rs
+++ impl/src/partial_eq.rs
@@ -2,7 +2,7 @@
 
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote, ToTokens};
-use syn::spanned::Spanned as _;
+use syn::{spanned::Spanned as _, token};
 
 /// Expands a [`PartialEq`] derive macro.
 pub fn expand(input: &syn::DeriveInput, _: &'static str) -> syn::Result<TokenStream> {
@@ -49,11 +49,85 @@ impl<'i> TryFrom<&'i syn::DeriveInput> for StructuralExpansion<'i> {
     }
 }
 
+impl StructuralExpansion<'_> {
+    /// Generates body of the [`PartialEq::eq()] method implementation.
+    fn eq_body(&self) -> TokenStream {
+        // Special case: empty enum.
+        if self.variants.is_empty() {
+            return quote! { match *self {} };
+        }
+        // Special case: no fields to compare.
+        if self.variants.len() == 1 && self.variants[0].1.is_empty() {
+            return quote! { true };
+        }
+
+        let discriminants_eq = (self.variants.len() > 1).then(|| {
+            quote! {
+                derive_more::core::mem::discriminant(self) ==
+                    derive_more::core::mem::discriminant(other)
+            }
+        });
+
+        let matched_variants = self
+            .variants
+            .iter()
+            .filter_map(|(variant, fields)| {
+                if fields.is_empty() {
+                    return None;
+                }
+                let variant = variant.map(|variant| quote! { :: #variant });
+                let self_pattern = fields.pattern("__self_");
+                let other_pattern = fields.pattern("__other_");
+                let eqs = (0..fields.len()).map(|num| {
+                    let self_val = format_ident!("__self_{num}");
+                    let other_val = format_ident!("__other_{num}");
+                    quote! { #self_val == #other_val }
+                });
+                Some(quote! {
+                    (Self #variant #self_pattern, Self #variant #other_pattern) => {
+                        #( #eqs )&&*
+                    }
+                })
+            })
+            .collect::<Vec<_>>();
+        let match_expr = (!matched_variants.is_empty()).then(|| {
+            let always_true_arm =
+                (matched_variants.len() != self.variants.len()).then(|| {
+                    quote! { _ => true }
+                });
+            let unreachable_arm = (self.variants.len() > 1
+                && always_true_arm.is_none())
+            .then(|| {
+                quote! {
+                    // SAFETY: This arm is never reachable, but is required by the expanded
+                    //         `match (self, other)` expression when there is more than one variant.
+                    _ => unsafe { derive_more::core::hint::unreachable_unchecked() },
+                }
+            });
+
+            quote! {
+                match (self, other) {
+                    #( #matched_variants , )*
+                    #always_true_arm
+                    #unreachable_arm
+                }
+            }
+        });
+
+        let and = (discriminants_eq.is_some() && match_expr.is_some())
+            .then_some(token::AndAnd::default());
+
+        quote! {
+            #discriminants_eq #and #match_expr
+        }
+    }
+}
+
 impl ToTokens for StructuralExpansion<'_> {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let ty = self.self_ty.0;
 
-        let mut generics = self.self_ty.1.clone();
+        let generics = self.self_ty.1.clone();
         /*if !generics.params.is_empty() {
             generics.make_where_clause().predicates.push(parse_quote! {
                 #inner_ty: derive_more::core::str::FromStr
@@ -61,49 +135,20 @@ impl ToTokens for StructuralExpansion<'_> {
         }*/
         let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-        let discriminants_eq = (self.variants.len() > 1).then(|| {
-            quote! {
-                derive_more::core::mem::discriminant(self) ==
-                    derive_more::core::mem::discriminant(other) &&
-            }
-        });
-        let fields_eqs = self.variants.iter().map(|(variant_name, fields)| {
-            let variant = variant_name.map(|i| quote! { :: #variant_name });
-            let self_pattern = fields.pattern("__self_");
-            let other_pattern = fields.pattern("__other_");
-            let eqs = (0..fields.len()).map(|num| {
-                let self_val = format_ident!("__self_{num}");
-                let other_val = format_ident!("__other_{num}");
-                quote! { #self_val == #other_val }
-            });
-            quote! {
-                (Self #variant #self_pattern, Self #variant #other_pattern) => {
-                    #( #eqs )&&*
-                }
-            }
-        });
-
-        let unreachable_arm = (self.variants.len() > 1).then(|| {
-            quote! {
-                // SAFETY: This arm is never reachable, but is required by the expanded
-                //         `match (self, other)` expression when there is more than one variant.
-                _ => unsafe { derive_more::core::intrinsics::unreachable() },
-            }
-        });
+        let eq_body = self.eq_body();
 
         quote! {
             #[automatically_derived]
-            impl #impl_generics derive_more::core::cmp::PartialEq for #ty #ty_generics #where_clause {
+            impl #impl_generics derive_more::core::cmp::PartialEq for #ty #ty_generics
+                 #where_clause
+            {
                 #[inline]
                 fn eq(&self, other: &Self) -> bool {
-                    #discriminants_eq
-                    match (self, other) {
-                        #( #fields_eqs , )*
-                        #unreachable_arm
-                    }
+                    #eq_body
                 }
             }
-        }.to_tokens(tokens);
+        }
+        .to_tokens(tokens);
     }
 }
 
@@ -129,7 +174,7 @@ impl FieldsExt for syn::Fields {
                 });
                 quote! {( #( #fields , )* )}
             }
-            Self::Unit => todo!(),
+            Self::Unit => unreachable!("empty fields should not be matched"),
         }
     }
 }
--- tests/partial_eq.rs
+++ tests/partial_eq.rs
@@ -1,35 +1,33 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#![allow(dead_code)] // some code is tested for type checking only
 
 mod structs {
     mod structural {
         use derive_more::PartialEq;
 
-        /*
         #[test]
         fn unit() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            struct Foo;
+            #[derive(Debug, PartialEq)]
+            struct Baz;
 
-            assert_eq!("Foo".parse::<Foo>().unwrap(), Foo);
+            assert_eq!(Baz, Baz);
         }
 
         #[test]
         fn empty_tuple() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            struct Bar();
+            #[derive(Debug, PartialEq)]
+            struct Foo();
 
-            assert_eq!("Bar".parse::<Bar>().unwrap(), Bar());
+            assert_eq!(Foo(), Foo());
         }
 
         #[test]
         fn empty_struct() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            struct Baz {}
+            #[derive(Debug, PartialEq)]
+            struct Bar {}
 
-            assert_eq!("Baz".parse::<Baz>().unwrap(), Baz {});
+            assert_eq!(Bar {}, Bar {});
         }
-
-         */
 
         #[test]
         fn multi_field_tuple() {
@@ -63,8 +61,44 @@ mod enums {
         use derive_more::PartialEq;
 
         #[test]
+        fn empty() {
+            #[derive(Debug, PartialEq)]
+            enum E {}
+        }
+
+        #[test]
+        fn single_variant_unit() {
+            #[derive(Debug, PartialEq)]
+            enum E {
+                Baz,
+            }
+
+            assert_eq!(E::Baz, E::Baz);
+        }
+
+        #[test]
+        fn single_variant_empty_tuple() {
+            #[derive(Debug, PartialEq)]
+            enum E {
+                Foo(),
+            }
+
+            assert_eq!(E::Foo(), E::Foo());
+        }
+
+        #[test]
+        fn single_variant_empty_struct() {
+            #[derive(Debug, PartialEq)]
+            enum E {
+                Bar {},
+            }
+
+            assert_eq!(E::Bar {}, E::Bar {});
+        }
+
+        #[test]
         fn single_variant_multi_field_tuple() {
-            #[derive(Debug, derive_more::PartialEq)]
+            #[derive(Debug, PartialEq)]
             enum E {
                 Foo(bool, i32),
             }
@@ -89,7 +123,30 @@ mod enums {
         }
 
         #[test]
-        fn multi_field() {
+        fn multi_variant_empty_field() {
+            #[derive(Debug, PartialEq)]
+            enum E {
+                Foo(),
+                Bar {},
+                Baz,
+            }
+
+            assert_eq!(E::Foo(), E::Foo());
+
+            assert_eq!(E::Bar {}, E::Bar {});
+
+            assert_eq!(E::Baz, E::Baz);
+
+            assert_ne!(E::Foo(), E::Bar {});
+            assert_ne!(E::Bar {}, E::Foo());
+            assert_ne!(E::Foo(), E::Baz);
+            assert_ne!(E::Baz, E::Foo());
+            assert_ne!(E::Bar {}, E::Baz);
+            assert_ne!(E::Baz, E::Bar {});
+        }
+
+        #[test]
+        fn multi_variant_multi_field() {
             #[derive(Debug, PartialEq)]
             enum E {
                 Foo(bool, i32),
@@ -110,79 +167,23 @@ mod enums {
             assert_ne!(E::Bar { b: false, i: 1 }, E::Foo(false, 1));
         }
 
-        /*
         #[test]
-        fn empty() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            enum Enum {}
-
-            assert_eq!(
-                "other".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
-        }
-
-        #[test]
-        fn unit() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            enum Enum {
-                Foo,
+        fn multi_variant_empty_and_multi_field() {
+            #[derive(Debug, derive_more::PartialEq)]
+            enum E {
+                Foo(bool, i32),
+                Baz,
             }
 
-            assert_eq!("Foo".parse::<Enum>().unwrap(), Enum::Foo);
-            assert_eq!("FOO".parse::<Enum>().unwrap(), Enum::Foo);
-            assert_eq!("foo".parse::<Enum>().unwrap(), Enum::Foo);
+            assert_eq!(E::Foo(true, 0), E::Foo(true, 0));
+            assert_ne!(E::Foo(true, 0), E::Foo(false, 0));
+            assert_ne!(E::Foo(true, 0), E::Foo(true, 1));
+            assert_ne!(E::Foo(true, 0), E::Foo(false, 1));
 
-            assert_eq!(
-                "baz".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
-            assert_eq!(
-                "other".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
+            assert_eq!(E::Baz, E::Baz);
+
+            assert_ne!(E::Foo(true, 0), E::Baz);
+            assert_ne!(E::Baz, E::Foo(false, 1));
         }
-
-        #[test]
-        fn empty_tuple() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            enum Enum {
-                Foo(),
-            }
-
-            assert_eq!("Foo".parse::<Enum>().unwrap(), Enum::Foo());
-            assert_eq!("FOO".parse::<Enum>().unwrap(), Enum::Foo());
-            assert_eq!("foo".parse::<Enum>().unwrap(), Enum::Foo());
-
-            assert_eq!(
-                "baz".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
-            assert_eq!(
-                "other".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
-        }
-
-        #[test]
-        fn empty_struct() {
-            #[derive(Debug, Eq, FromStr, PartialEq)]
-            enum Enum {
-                Foo {},
-            }
-
-            assert_eq!("Foo".parse::<Enum>().unwrap(), Enum::Foo {});
-            assert_eq!("FOO".parse::<Enum>().unwrap(), Enum::Foo {});
-            assert_eq!("foo".parse::<Enum>().unwrap(), Enum::Foo {});
-
-            assert_eq!(
-                "baz".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
-            assert_eq!(
-                "other".parse::<Enum>().unwrap_err().to_string(),
-                "Invalid `Enum` string representation",
-            );
-        }*/
     }
 }
