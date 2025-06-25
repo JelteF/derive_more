ffafb1ee12e1d2ed6e4b68f4cfc5ab592ecfddc1
Kien Dang <kiend@proton.me>
2025-06-25 18:37:33 +0800

    Add more tests for enum Deref

--- tests/deref.rs
+++ tests/deref.rs
@@ -96,36 +96,107 @@ enum MyEnum<'a> {
 }
 
 #[derive(Deref)]
-enum Compression {
-    Stored(u32),
-    Zlib(#[deref(forward)] Box<u32>),
-    LZMA1(u32),
+#[deref(forward)]
+enum MyBoxedIntEnum {
+    Variant(Box<i32>),
+}
+
+#[derive(Deref)]
+#[deref(forward)]
+enum NumRefEnum<'a> {
+    Variant1 {
+        num: &'a i32,
+    },
+    Variant2(&'a i32),
+    Variant3 {
+        n: &'a i32,
+        #[deref(ignore)]
+        useless: bool,
+    },
+}
+
+#[derive(Deref)]
+enum NumRefEnum2<'a> {
+    Variant1 {
+        #[deref]
+        num: &'a i32,
+        useless: bool,
+    },
+    Variant2 {
+        num: &'a i32,
+        #[deref(ignore)]
+        useless: bool,
+    },
+    Variant3(&'a i32),
+}
+
+#[derive(Deref)]
+enum NumRefEnum3 {
+    Variant1 {
+        #[deref(forward)]
+        num: Box<i32>,
+        useless: bool,
+    },
+    Variant2 {
+        num: i32,
+        #[deref(ignore)]
+        useless: bool,
+    },
+    Variant3(i32),
+}
+
+#[derive(Deref)]
+enum NumRefEnum4 {
+    Variant1(i32),
+    Variant2 {
+        #[deref(forward)]
+        num: Box<i32>,
+        useless: bool,
+    },
+    Variant3 {
+        num: i32,
+        #[deref(ignore)]
+        useless: bool,
+    },
+}
+
+#[derive(Deref)]
+enum GenericBoxEnum1<T> {
+    Variant1 {
+        #[deref(forward)]
+        b: Box<T>,
+    },
+    Variant2(bool, #[deref(forward)] Box<T>),
 }
 
 #[test]
-fn deref_enum() {
-    let e = Compression::Stored(5);
-    assert_eq!(*e, 5);
+fn deref_generic_forward_enum_inner() {
+    let boxed = GenericBoxEnum1::Variant2(true, Box::new(1i32));
+    assert_eq!(*boxed, 1i32);
 }
 
 #[derive(Deref)]
-enum MyEnum1 {
+enum GenericBoxEnum2<T> {
     Variant1 {
-        #[deref]
-        named_field1: u8,
-        named_field2: bool,
+        a: T,
+        #[deref(forward)]
+        b: Box<i32>,
     },
-    Variant2 {
-        named_field1: u8,
-        #[deref(ignore)]
-        named_field2: bool,
-    },
+    Variant2(bool, #[deref(forward)] Box<i32>),
 }
 
 #[derive(Deref)]
-enum MyEnum2 {
-    Variant1(u8, #[deref(ignore)] bool),
-    Variant2(#[deref] u8, bool),
+#[deref(forward)]
+enum GenericBoxEnum3<T> {
+    Variant1 { b: Box<T> },
+    Variant2(Box<T>),
+    Variant3(#[deref(ignore)] bool, Box<T>),
+}
+
+#[test]
+fn deref_generic_forward_enum_outer() {
+    let boxed = GenericBoxEnum3::Variant2(Box::new(1i32));
+    assert_eq!(*boxed, 1i32);
 }
 
 mod deprecated {
