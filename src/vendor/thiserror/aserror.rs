1b4e081d70f3a2e074f23024e8945d7dc799ce37
AlphaKeks <alphakeks@dawn.sh>
2025-12-19 12:13:23 +0100

    Disambiguate `.as_dyn_error()` in `#[derive(Error)]`
    Fixes #526

--- src/vendor/thiserror/aserror.rs
+++ src/vendor/thiserror/aserror.rs
@@ -2,40 +2,40 @@ use core::{error::Error, panic::UnwindSafe};
 
 #[doc(hidden)]
 pub trait AsDynError<'a>: Sealed {
-    fn as_dyn_error(&self) -> &(dyn Error + 'a);
+    fn __as_dyn_error(&self) -> &(dyn Error + 'a);
 }
 
 impl<'a, T: Error + 'a> AsDynError<'a> for T {
     #[inline]
-    fn as_dyn_error(&self) -> &(dyn Error + 'a) {
+    fn __as_dyn_error(&self) -> &(dyn Error + 'a) {
         self
     }
 }
 
 impl<'a> AsDynError<'a> for dyn Error + 'a {
     #[inline]
-    fn as_dyn_error(&self) -> &(dyn Error + 'a) {
+    fn __as_dyn_error(&self) -> &(dyn Error + 'a) {
         self
     }
 }
 
 impl<'a> AsDynError<'a> for dyn Error + Send + 'a {
     #[inline]
-    fn as_dyn_error(&self) -> &(dyn Error + 'a) {
+    fn __as_dyn_error(&self) -> &(dyn Error + 'a) {
         self
     }
 }
 
 impl<'a> AsDynError<'a> for dyn Error + Send + Sync + 'a {
     #[inline]
-    fn as_dyn_error(&self) -> &(dyn Error + 'a) {
+    fn __as_dyn_error(&self) -> &(dyn Error + 'a) {
         self
     }
 }
 
 impl<'a> AsDynError<'a> for dyn Error + Send + Sync + UnwindSafe + 'a {
     #[inline]
-    fn as_dyn_error(&self) -> &(dyn Error + 'a) {
+    fn __as_dyn_error(&self) -> &(dyn Error + 'a) {
         self
     }
 }
