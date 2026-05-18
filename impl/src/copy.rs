5350237b7820636c9ea88f28ae45c9bdf006e52d
Thibaut Lorrain <thibaut.lorrain@stockly.ai>
2026-05-18 18:07:35 +0200

    do now overwrite the where clause from the struct declaration

--- impl/src/copy.rs
+++ impl/src/copy.rs
@@ -1,23 +1,23 @@
 use crate::utils::attr::{self, ParseMultiple};
 use proc_macro2::TokenStream;
-use quote::{quote, ToTokens};
+use quote::quote;
 use syn::DeriveInput;
 
 pub fn expand(input: &DeriveInput, _: &'static str) -> syn::Result<TokenStream> {
     let name = &input.ident;
-    let generics = &input.generics;
+    let ident = quote::format_ident!("copy");
+
+    let mut generics = input.generics.clone();
+    if let Some(bounds) = attr::Bounds::parse_attrs(&input.attrs, &ident)? {
+        generics
+            .make_where_clause()
+            .predicates
+            .extend(bounds.item.0);
+    }
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-    let ident = quote::format_ident!("copy");
-    let real_where_clause = attr::Bounds::parse_attrs(&input.attrs, &ident)?
-        .map(|clause| {
-            let clause = clause.item.0;
-            quote! { where #clause }
-        })
-        .or(where_clause.map(ToTokens::to_token_stream));
-
     Ok(quote! {
-        impl #impl_generics derive_more::core::marker::Copy for #name #ty_generics #real_where_clause {
+        impl #impl_generics derive_more::core::marker::Copy for #name #ty_generics #where_clause {
 
         }
     })
