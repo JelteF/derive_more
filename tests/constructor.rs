53b88c0b334203ec2629cad6fb130266fe9b2849
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2026-06-01 08:57:45 -0600

    test: Add tests for `Into` constructors

--- tests/constructor.rs
+++ tests/constructor.rs
@@ -2,6 +2,11 @@
 #![cfg_attr(nightly, feature(never_type))]
 #![allow(dead_code)] // some code is tested for type checking only
 
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+#[cfg(not(feature = "std"))]
+use alloc::string::String;
+
 use derive_more::Constructor;
 
 #[derive(Constructor)]
@@ -23,6 +28,41 @@ const EMPTY_UNIT: EmptyUnit = EmptyUnit::new();
 struct MyInts(i32, i32);
 
 const MY_INTS: MyInts = MyInts::new(1, 2);
+
+#[derive(Constructor)]
+struct IntoTuple(#[constructor(into)] String);
+
+#[derive(Constructor)]
+struct IntoStruct {
+    #[constructor(into)]
+    value: String,
+}
+
+#[derive(Constructor)]
+struct IntoMixed {
+    id: i32,
+    #[constructor(into)]
+    name: String,
+}
+
+#[test]
+fn into_arguments_are_converted() {
+    // Accepts any `Into<String>`
+    let tuple = IntoTuple::new("tuple");
+    assert_eq!(tuple.0, "tuple");
+
+    let named = IntoStruct::new("named");
+    assert_eq!(named.value, "named");
+
+    // Non-`Into<...>` fields keep requiring their exact type.
+    let mixed = IntoMixed::new(1, "mixed");
+    assert_eq!(mixed.id, 1);
+    assert_eq!(mixed.name, "mixed");
+
+    // An owned `String` should still work
+    let owned = IntoTuple::new(String::from("owned"));
+    assert_eq!(owned.0, "owned");
+}
 
 #[derive(Constructor)]
 struct Point2D {
